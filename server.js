4cf2e2dede46968c56ccfaef321d76f6f7ac0561
axeljadr <julioaxeldelgadorivera@gmail.com>
2025-11-30 18:32:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/static", express.static(path.join(__dirname, "static")));
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
